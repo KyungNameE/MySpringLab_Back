c85886dbe373e3dae9ca33f0fdf809c25da33517
paks <w.tyu7766@gmail.com>
2025-02-04 16:52:17 +0900

    asdasd

---
+++ src/test/java/com/example/myspringlab/MySpringLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myspringlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
